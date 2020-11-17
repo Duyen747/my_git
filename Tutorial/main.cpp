34d0df72eb31ead83685bd219dc6e93dbbc9ad92
Duyen Ho Le Nhu <holenhuduyen@gmail.com>
2020-11-17 12:52:17 +0700

    add Tutorial project

--- Tutorial/main.cpp
+++ Tutorial/main.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
+#include "Figure.h"
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	cout << "Hello LG and Genetic" << endl;
+	Point2D P;
+	P.Set(7.2, 4.3);
+	P.Move(2.7, 4.1);
+	
+	Circle Cir;
+	Cir.Set(1.0, 1.0, 3);
+	double area = Cir.Area();
+	double Perimeter  = Cir.Perimeter();
+	
+	cout << "Area: " << area << endl;
+	cout << "Perimeter: " << Perimeter << endl;
 	return 0;
 }
